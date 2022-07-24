d4a3c0c28fe2f4bcf2ec73f5dee45f928caafa5f
darkshenu5 <108314701+darkshenu5@users.noreply.github.com>
2022-07-23 19:31:47 -0700

    Update Mr_Robot.js

--- Mr_Robot.js
+++ Mr_Robot.js
@@ -2126,10 +2126,10 @@ break
 	     case 'document': {
 		 let { yta } = require('./lib/y2mate')
                 if (!text) return reply(`Example : ${prefix + command} https://youtube.com/watch?v=PtFMh6Tccag%27 128kbps`)
-                let quality = args[1] ? args[1] : '128kbps'
+                let quality = args[1] ? args[1] : '320kbps'
                 let media = await yta(text, quality)
                 if (media.filesize >= 999999) return reply('File Over Limit '+util.format(media))
-                Robot_MD.sendImage(m.chat, media.thumb, `🐦 Title : ${media.title}\n🐦 File Size : ${media.filesizeF}\n🐦 Url : ${isUrl(text)}\n🐦 Ext : MP3\n🐦 Resolution : ${args[1] || '128kbps'}`, m)
+                Robot_MD.sendImage(m.chat, media.thumb, `🐦 Title : ${media.title}\n🐦 File Size : ${media.filesizeF}\n🐦 Url : ${isUrl(text)}\n🐦 Ext : MP3\n🐦 Resolution : ${args[1] || '320kbps'}`, m)
                 Robot_MD.sendMessage(m.chat, { audio: { url: media.dl_link }, mimetype: 'document/mpeg', fileName: `${media.title}.mp3` }, { quoted: m })
 	    }
 	    break
