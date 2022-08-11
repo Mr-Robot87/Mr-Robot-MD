760e75b0136527e68b40bb7018c79704192e5cf8
Misara Sathasara <108314701+Mr-Robot87@users.noreply.github.com>
2022-08-11 19:18:02 +0530

    Update Mr_Robot.js

--- Mr_Robot.js
+++ Mr_Robot.js
@@ -2048,16 +2048,20 @@ break
         }
         break
 	    
-            case 'play': case 'song': case 'ytplay': {
+            case 'play': case 'song': case 'ytplay'case 'ytmp3': case 'getmusic': case 'ytaudio' : {
 		Robot_MD.sendMessage(from, { react: { text: `👀`, key: m.key }})
                 if (!text) return reply(`Example : ${prefix + command} ?????? ???? ???? ????`)
                 let yts = require("yt-search")
                 let search = await yts(text)
                 let anu = search.videos[0]
-                let buttons = [
-                    {buttonId: `ytmp3 ${anu.url}`, buttonText: {displayText: '♬Audio♬'}, type: 1},
-                    
-                  {buttonId: `document ${anu.url}`, buttonText: {displayText: '♬Document♬'}, type: 1}] 
+		 let { yta } = require('./lib/y2mate')
+                if (!text) return reply(`Example : ${prefix + command} https://youtube.com/watch?v=PtFMh6Tccag%27 256kbps`)
+                let quality = args[1] ? args[1] : '256kbps'
+                let media = await yta(text, quality)
+                if (media.filesize >= 999999) return reply('File Over Limit '+util.format(media))
+                Robot_MD.sendImage(m.chat, media.thumb, `♪♪ Title : ${media.title}\n📌 File Size : ${media.filesizeF}\n📝 Url : ${isUrl(text)}\n🎶 Ext  : MP3\n📢 📢 Resolution  : ${args[1] || '256kbps'}`, m)
+                Robot_MD.sendMessage(m.chat, { audio: { url: media.dl_link }, mimetype: 'audio/mpeg', fileName: `${media.title}.mp3` }, { quoted: m })
+                
                     
                   
                 
@@ -2085,6 +2089,7 @@ break
                 let yts = require("yt-search")
                 let search = await yts(text)
                 let anu = search.videos[0]
+		
                 let buttons = [
                     {buttonId: `ytmp4 ${anu.url}`, buttonText: {displayText: '480p'}, type: 1},
                     
