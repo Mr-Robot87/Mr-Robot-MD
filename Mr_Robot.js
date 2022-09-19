e4d1114b3c6a76a2d707ad42ccadd7dfde891c35
Misara Sathasara <108314701+Mr-Robot87@users.noreply.github.com>
2022-09-19 08:07:24 +0530

    Update Mr_Robot.js

--- Mr_Robot.js
+++ Mr_Robot.js
@@ -222,11 +222,7 @@ const reactionMessage = {
             if (!m.key.fromMe) return
         }
 
-        //Push Message To Console && Auto Read\\
-        if (m.message) {
-            Robot_MD.sendReadReceipt(m.chat, m.sender, [m.key.id])
-            console.log(chalk.black(chalk.bgWhite('[ MESSAGE ]')), chalk.black(chalk.bgGreen(new Date)), chalk.black(chalk.bgBlue(budy || m.mtype)) + '\n' + chalk.magenta('=> From'), chalk.green(pushname), chalk.yellow(m.sender) + '\n' + chalk.blueBright('=> In'), chalk.green(m.isGroup ? pushname : 'Private Chat', m.chat))
-        }
+        
 	
 	//reset limit every 12 hours\\
         let cron = require('node-cron')
